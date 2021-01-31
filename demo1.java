4f880a80a3116ffb3dd11994e66837ae9588cd55
gupta25061005 <78317117+gupta25061005@users.noreply.github.com>
2021-01-31 23:40:53 +0530

    Update demo1.java

--- demo1.java
+++ demo1.java
@@ -5,6 +5,9 @@ public static void main(String args[])
 {
 System.out.println(" java demo1 class");
 System.out.println(" java demo1 class1 2nd line");
+ System.out.println(" java demo1 class1 2nd line");
+System.out.println(" java demo1 class1 2nd line");
+
 }
 
 
